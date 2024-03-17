a2c61be38d9224fa93553480076738b861725c86
TenHenry <marcinuznanski2@gmail.com>
2024-03-17 22:53:35 +0100

    Dodalem_cos

---
+++ Desktop/p1/Zadanie_komputerowe_git.sh
@@ -0,0 +1,21 @@
+1.git commit
+2.git checkout
+3.git merge
+4.git rebase
+5.git checkout HASZ
+6.git checkout HEAD^
+7.git branch
+8.git reset; git revert
+9.git cherry-pick
+10.git rebase -i
+11.git tag
+12.git describe
+13.git checkout HEAD~^2~
+
+1)git clone
+2)git fetch
+3)git pull
+4)git push
+6)git fakeTeamwork
+5)git pull --rebase
+6)git push origin 
